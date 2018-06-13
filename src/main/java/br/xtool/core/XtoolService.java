1e32440c89d6262adce6506efcb11775f8db78d7
jcruz <jcruz@tre-pa.jus.br>
2018-06-13 19:00:10 -0300

    Impl do comando cd

--- src/main/java/br/xtool/core/XtoolService.java
+++ src/main/java/br/xtool/core/XtoolService.java
@@ -1,9 +1,12 @@
 package br.xtool.core;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import lombok.Getter;
-import lombok.Setter;
 
 /**
  * Bean com as informações de contexto da aplicação.
@@ -13,11 +16,23 @@ import lombok.Setter;
  */
 @Service
 @Getter
-@Setter
 public class XtoolService {
 
 	/**
 	 * Diretório atual
 	 */
-	private String currentDirectory;
+	private String workingDirectory;
+
+	/**
+	 * Altera o diretório de trabalho.
+	 * 
+	 * @param dir
+	 */
+	public void changeWorkingDirectory(String dir) {
+		if (Files.exists(Paths.get(dir))) {
+			this.workingDirectory = dir;
+		}
+		throw new IllegalArgumentException("Diretório inválido. " + dir);
+	}
+
 }
--- src/main/java/br/xtool/core/command/CdCommand.java
+++ src/main/java/br/xtool/core/command/CdCommand.java
@@ -1,16 +1,21 @@
 package br.xtool.core.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
 import br.xtool.XtoolCliApplication;
+import br.xtool.core.XtoolService;
 
 @ShellComponent
 public class CdCommand {
 
+	@Autowired
+	private XtoolService xtoolService;
+
 	@ShellMethod(value = "Altera o diretório de trabalho do xtool", group = XtoolCliApplication.CORE_COMMAND_GROUP)
 	public void cd(@ShellOption(help = "Diretório alvo") String dir) {
-
+		xtoolService.changeWorkingDirectory(dir);
 	}
 }
