2337f5ba0ce18c0ff605360850f10d04f17b7373
jcruz <jcruz@tre-pa.jus.br>
2018-07-05 23:50:47 -0300

    Ajustes

--- src/main/java/br/xtool/core/representation/enums/ProjectType.java
+++ src/main/java/br/xtool/core/representation/enums/ProjectType.java
@@ -1,9 +1,22 @@
 package br.xtool.core.representation.enums;
 
 public enum ProjectType {
-	SPRINGBOOT1_PROJECT,
-	SPRINGBOOT2_PROJECT,
-	ANGULAR5_PROJECT, 
-	ANGULAR6_PROJECT,
-	NONE
+	// @formatter:off
+	SPRINGBOOT1_PROJECT("Projeto Spring Boot v1.5.x"),
+	SPRINGBOOT2_PROJECT("Projeto Spring Boot v2.x.x"),
+	ANGULAR5_PROJECT("Projeto Angular v5"), 
+	ANGULAR6_PROJECT("Projeto Angular v6"),
+	NONE("");
+	// @formatter:on
+
+	private String detail;
+
+	private ProjectType(String detail) {
+		this.detail = detail;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
 }
