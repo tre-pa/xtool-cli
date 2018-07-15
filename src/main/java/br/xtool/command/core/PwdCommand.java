6820e8feae37121f49d73064ff4e0f2a2545c39f
jcruz <jcruz@tre-pa.jus.br>
2018-07-15 14:33:14 -0300

    Ajustes nos comandos

--- src/main/java/br/xtool/command/core/PwdCommand.java
+++ src/main/java/br/xtool/command/core/PwdCommand.java
@@ -1,4 +1,4 @@
-package br.xtool.core.command;
+package br.xtool.command.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
