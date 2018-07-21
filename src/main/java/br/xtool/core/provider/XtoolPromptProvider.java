00357732a1123d47b5e623708cbffb7272ade58c
jcruz <jcruz@tre-pa.jus.br>
2018-07-20 22:25:20 -0300

    Ajustes

--- src/main/java/br/xtool/core/provider/XtoolPromptProvider.java
+++ src/main/java/br/xtool/core/provider/XtoolPromptProvider.java
@@ -4,26 +4,28 @@ import java.util.Objects;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
-import org.springframework.context.event.EventListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
 
-import br.xtool.core.event.ChangeWorkingProjectEvent;
-import br.xtool.core.representation.EProject;
+import br.xtool.core.service.WorkspaceService;
 
 @Component
 public class XtoolPromptProvider implements PromptProvider {
 
-	private EProject workingProject;
+	//	private EProject workingProject;
+
+	@Autowired
+	private WorkspaceService workspaceService;
 
 	@Override
 	public AttributedString getPrompt() {
-		String promptLabel = Objects.nonNull(this.workingProject) ? String.format("xtool/%s ~ ", this.workingProject.getName()) : "xtool ~ ";
+		String promptLabel = Objects.nonNull(this.workspaceService.getWorkingProject()) ? String.format("xtool/%s ~ ", this.workspaceService.getWorkingProject().getName()) : "xtool ~ ";
 		return new AttributedString(promptLabel, AttributedStyle.DEFAULT.bold().foreground(AttributedStyle.YELLOW));
 	}
 
-	@EventListener
-	private void onChangeWorkingProject(ChangeWorkingProjectEvent evt) {
-		this.workingProject = evt.getProject();
-	}
+	//	@EventListener
+	//	private void onChangeWorkingProject(ChangeWorkingProjectEvent evt) {
+	//		this.workingProject = evt.getProject();
+	//	}
 }
