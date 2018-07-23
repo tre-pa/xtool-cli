d947e66a7d63d6b49de04318c983050743bf9310
jcruz <jcruz@tre-pa.jus.br>
2018-07-23 13:09:26 -0300

    Ajustes

--- src/main/java/br/xtool/core/representation/EJavaSourceFolder.java
+++ src/main/java/br/xtool/core/representation/EJavaSourceFolder.java
@@ -1,6 +1,5 @@
 package br.xtool.core.representation;
 
-import java.nio.file.Path;
 import java.util.SortedSet;
 
 /**
@@ -9,14 +8,7 @@ import java.util.SortedSet;
  * @author jcruz
  *
  */
-public interface EJavaSourceFolder {
-
-	/**
-	 * Retorna o caminho do source folder java.
-	 * 
-	 * @return
-	 */
-	Path getPath();
+public interface EJavaSourceFolder extends ESourceFolder {
 
 	/**
 	 * Retorna a lista de pacotes do source folder java.
---
+++ src/main/java/br/xtool/core/representation/ESourceFolder.java
@@ -0,0 +1,14 @@
+package br.xtool.core.representation;
+
+import java.nio.file.Path;
+
+public interface ESourceFolder {
+
+	/**
+	 * Retorna o caminho do source folder.
+	 * 
+	 * @return
+	 */
+	Path getPath();
+
+}
