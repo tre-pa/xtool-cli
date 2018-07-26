469d27465c9670f9164acb57f0b55c0913bbc929
jcruz <jcruz@tre-pa.jus.br>
2018-07-26 09:51:45 -0300

    Ajuste no enum Version

--- src/main/java/br/xtool/core/representation/EProject.java
+++ src/main/java/br/xtool/core/representation/EProject.java
@@ -3,6 +3,9 @@ package br.xtool.core.representation;
 import java.nio.file.Path;
 import java.util.Collection;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Interface genérica para projetos.
  * 
@@ -27,8 +30,11 @@ public interface EProject extends Comparable<EProject> {
 	 * @author jcruz
 	 *
 	 */
+	@AllArgsConstructor
+	@Getter
 	enum Version {
-		V1, V2, V3, V4, V5, V6, V7, V8, V9, V10
+		V1("v1"), V2("v2"), V3("v3"), V4("v4"), V5("v5"), V6("v6"), V7("v7"), V8("v8"), V9("v9"), V10("v10");
+		private String name;
 	}
 
 	Path getPath();
