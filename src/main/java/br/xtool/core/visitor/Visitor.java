150d89ac077be9cd72409f1432f4837d7a339017
jcruz <jcruz@tre-pa.jus.br>
2018-07-30 23:49:15 -0300

    add comment

--- src/main/java/br/xtool/core/visitor/Visitor.java
+++ src/main/java/br/xtool/core/visitor/Visitor.java
@@ -7,30 +7,95 @@ import br.xtool.core.representation.EUmlRelationship;
 
 public interface Visitor {
 
+	/**
+	 * Visita a classe UML.
+	 * 
+	 * @param umlClass
+	 */
 	void visitClass(EUmlClass umlClass);
 
+	/**
+	 * Visita o atributo Id.
+	 * 
+	 * @param umlField
+	 */
 	void visitIdField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo Long.
+	 * 
+	 * @param umlField
+	 */
 	void visitLongField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo Boolean.
+	 * 
+	 * @param umlField
+	 */
 	void visitBooleanField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo String.
+	 * 
+	 * @param umlField
+	 */
 	void visitStringField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo Integer.
+	 * 
+	 * @param umlField
+	 */
 	void visitIntegerField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo BigDecimal.
+	 * 
+	 * @param umlField
+	 */
 	void visitBigDecimalField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo Array de bytes.
+	 * 
+	 * @param umlField
+	 */
 	void visitByteArrayField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo LocalDate.
+	 * 
+	 * @param umlField
+	 */
 	void visitLocalDateField(EUmlField umlField);
 
+	/**
+	 * Visita o atributo do tipo LocalDateTime.
+	 * 
+	 * @param umlField
+	 */
 	void visitLocalDateTimeField(EUmlField umlField);
 
+	/**
+	 * Visita atributo com propriedade unique.
+	 * 
+	 * @param umlFieldProperty
+	 */
 	void visitUniqueProperty(EUmlFieldProperty umlFieldProperty);
 
+	/**
+	 * Visita atributo com propriedade notNull.
+	 * 
+	 * @param umlFieldProperty
+	 */
 	void visitNotNullProperty(EUmlFieldProperty umlFieldProperty);
 
+	/**
+	 * Visita atributo com propriedade Transient.
+	 * 
+	 * @param umlFieldProperty
+	 */
 	void visitTransientProperty(EUmlFieldProperty umlFieldProperty);
 
 	void visit(EUmlRelationship umlRelationship);
