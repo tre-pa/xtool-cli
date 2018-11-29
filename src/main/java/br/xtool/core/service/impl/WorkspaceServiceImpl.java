00129ca6107d1802108d8e44cfa42c4b63cf0579
jcruz <jcruz@tre-pa.jus.br>
2018-11-29 10:34:20 -0300

    First commit do archetype v7-dx

--- src/main/java/br/xtool/core/service/impl/WorkspaceServiceImpl.java
+++ src/main/java/br/xtool/core/service/impl/WorkspaceServiceImpl.java
@@ -7,11 +7,13 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import br.xtool.core.representation.EProject;
+import br.xtool.core.representation.EProject.Type;
 import br.xtool.core.representation.EProject.Version;
 import br.xtool.core.representation.EWorkspace;
 import br.xtool.core.representation.impl.ENoneProjectImpl;
@@ -40,6 +42,7 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see br.xtool.core.service.WorkspaceService#getWorkspace()
 	 */
 	@Override
@@ -49,7 +52,9 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 
 	/*
 	 * (non-Javadoc)
-	 * @see br.xtool.core.service.WorkspaceService#setWorkingProject(br.xtool.core.representation.EProject)
+	 * 
+	 * @see br.xtool.core.service.WorkspaceService#setWorkingProject(br.xtool.core.
+	 * representation.EProject)
 	 */
 	@Override
 	public void setWorkingProject(EProject project) {
@@ -58,7 +63,9 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 
 	/*
 	 * (non-Javadoc)
-	 * @see br.xtool.core.service.WorkspaceService#getWorkingProject(java.lang.Class)
+	 * 
+	 * @see
+	 * br.xtool.core.service.WorkspaceService#getWorkingProject(java.lang.Class)
 	 */
 	@Override
 	public <T extends EProject> T getWorkingProject(Class<T> projectClass) {
@@ -67,6 +74,7 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see br.xtool.core.service.WorkspaceService#createDirectory(java.lang.String)
 	 */
 	@Override
@@ -81,11 +89,23 @@ public class WorkspaceServiceImpl implements WorkspaceService {
 
 	/*
 	 * (non-Javadoc)
-	 * @see br.xtool.core.service.WorkspaceService#createProject(java.lang.Class, java.lang.String, br.xtool.core.representation.EProject.Version)
+	 * 
+	 * @see br.xtool.core.service.WorkspaceService#createProject(java.lang.Class,
+	 * java.lang.String, br.xtool.core.representation.EProject.Version)
 	 */
 	@Override
 	public <T extends EProject> T createProject(Class<T> projectClass, EProject.Type type, String name, Version version, Map<String, Object> vars) {
 		Path archetypePath = Paths.get(type.getName()).resolve(version.getName()).resolve("archetype");
+		Path projectPath = this.createDirectory(name);
+		this.fs.copy(archetypePath, vars, projectPath);
+		return projectClass.cast(EProject.factory(projectClass, projectPath));
+	}
+
+	@Override
+	public <T extends EProject> T createProject(Class<T> projectClass, Type type, String name, Version version, String qualifier, Map<String, Object> vars) {
+		if (StringUtils.isEmpty(qualifier))
+			return this.createProject(projectClass, type, name, version, vars);
+		Path archetypePath = Paths.get(type.getName()).resolve(version.getName().concat("-").concat(qualifier)).resolve("archetype");
 		Path projectPath = this.createDirectory(name);
 		this.fs.copy(archetypePath, vars, projectPath);
 		return projectClass.cast(EProject.factory(projectClass, projectPath));
---
+++ src/main/resources/templates/angular/v7-dx/archetype/src/app/app.module.ts.vm
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SideNavOuterToolbarModule, SideNavInnerToolbarModule } from './layouts';
+import { FooterModule } from './shared/components/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SideNavOuterToolbarModule,
+    SideNavInnerToolbarModule,
+    FooterModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
