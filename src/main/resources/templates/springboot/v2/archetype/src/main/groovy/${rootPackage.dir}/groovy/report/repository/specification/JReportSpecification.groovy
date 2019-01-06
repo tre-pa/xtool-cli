9ba4dc1eed5e16cc3ec84c4c65076d4deb589f6d
jcruz <jcruz@tre-pa.jus.br>
2019-01-06 11:47:47 -0300

    Ajuste no código groovy

--- src/main/resources/templates/springboot/v2/archetype/src/main/groovy/${rootPackage.dir}/groovy/report/repository/specification/JReportSpecification.groovy
+++
@@ -1,8 +0,0 @@
-package br.jus.tre_pa.springboot.v2.template.service.groovy.report.repository.specification
-
-import br.jus.tre_pa.springboot.v2.template.service.groovy.qy.QySpecification
-import br.jus.tre_pa.springboot.v2.template.service.groovy.report.domain.JReport
-
-class JReportSpecification extends QySpecification<JReport> {
-
-}
