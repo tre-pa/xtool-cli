301162ef22477f92ae9bff738eff6edfc721e3cc
jcruz <jcruz@tre-pa.jus.br>
2019-02-08 14:57:59 -0300

    Organização de pacotes

--- src/main/java/br/xtool/core/XtoolPromptProvider.java
+++ src/main/java/br/xtool/core/XtoolPromptProvider.java
@@ -1,4 +1,4 @@
-package br.xtool.core.provider;
+package br.xtool.core;
 
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
 
-import br.xtool.core.Workspace;
 import br.xtool.core.representation.ProjectRepresentation.Type;
 
 @Component
