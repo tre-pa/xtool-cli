61ada090d87e4c91796f1144a35f929276c60c84
jcruz <jcruz@localhost>
2019-07-05 23:22:59 -0300

    Refactor no AngularService

--- src/main/java/br/xtool/core/implementation/service/AngularServiceImpl.java
+++ src/main/java/br/xtool/core/implementation/service/AngularServiceImpl.java
@@ -1,38 +1,25 @@
 package br.xtool.core.implementation.service;
 
-import java.io.BufferedWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.PrettyPrinter;
-import com.fasterxml.jackson.core.util.DefaultIndenter;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 
 import br.xtool.core.Clog;
 import br.xtool.core.FS;
 import br.xtool.core.Shell;
 import br.xtool.core.Workspace;
 import br.xtool.core.helper.InflectorHelper;
-import br.xtool.core.helper.JsonHelper;
-import br.xtool.core.helper.StringHelper;
-import br.xtool.core.helper.TemplateBuilder;
+import br.xtool.core.helper.NgHelper;
 import br.xtool.core.implementation.representation.NgCrudRepresentationImpl;
 import br.xtool.core.implementation.representation.NgDetailRepresentationImpl;
 import br.xtool.core.implementation.representation.NgEditRepresentationImpl;
@@ -42,7 +29,6 @@ import br.xtool.core.implementation.representation.NgListRepresentationImpl;
 import br.xtool.core.implementation.representation.NgServiceRepresentationImpl;
 import br.xtool.core.representation.ProjectRepresentation;
 import br.xtool.core.representation.angular.NgClassRepresentation;
-import br.xtool.core.representation.angular.NgComponentRepresentation;
 import br.xtool.core.representation.angular.NgCrudRepresentation;
 import br.xtool.core.representation.angular.NgDetailRepresentation;
 import br.xtool.core.representation.angular.NgEditRepresentation;
@@ -50,8 +36,6 @@ import br.xtool.core.representation.angular.NgEntityRepresentation;
 import br.xtool.core.representation.angular.NgEnumRepresentation;
 import br.xtool.core.representation.angular.NgListRepresentation;
 import br.xtool.core.representation.angular.NgModuleRepresentation;
-import br.xtool.core.representation.angular.NgPageNavigationRepresentation;
-import br.xtool.core.representation.angular.NgPageRepresentation;
 import br.xtool.core.representation.angular.NgProjectRepresentation;
 import br.xtool.core.representation.angular.NgRoute;
 import br.xtool.core.representation.angular.NgServiceRepresentation;
@@ -62,7 +46,6 @@ import br.xtool.core.template.angular.NgDetailDxTemplates;
 import br.xtool.core.template.angular.NgEditDxTemplates;
 import br.xtool.core.template.angular.NgListDxTemplates;
 import br.xtool.service.AngularService;
-import lombok.SneakyThrows;
 import strman.Strman;
 
 @Service
@@ -212,15 +195,15 @@ public class AngularServiceImpl implements AngularService {
 		NgEditRepresentation ngEdit = entity.isImmutable() ? null : genNgEdit(ngProject, entity, ngModule);
 		if (Objects.nonNull(ngEdit)) {
 			NgCrudRepresentation ngCrud = new NgCrudRepresentationImpl(ngModule, ngList, ngDetail, ngEdit);
-			addComponent(ngModule, ngCrud);
+			NgHelper.addComponent(ngModule, ngCrud);
 			addToRoute(ngModule, ngCrud);
-			ngModule.getAssociatedPage().ifPresent(ngPage -> addNavigation(ngPage, ngCrud));
+			ngModule.getAssociatedPage().ifPresent(ngPage -> NgHelper.addNavigation(ngPage, ngCrud));
 			return ngCrud;
 		}
 		NgCrudRepresentation ngCrud = new NgCrudRepresentationImpl(ngModule, ngList, ngDetail);
-		addComponent(ngModule, ngCrud);
+		NgHelper.addComponent(ngModule, ngCrud);
 		addToRoute(ngModule, ngCrud);
-		ngModule.getAssociatedPage().ifPresent(ngPage -> addNavigation(ngPage, ngCrud));
+		ngModule.getAssociatedPage().ifPresent(ngPage -> NgHelper.addNavigation(ngPage, ngCrud));
 		return ngCrud;
 	}
 
@@ -253,7 +236,7 @@ public class AngularServiceImpl implements AngularService {
 		ngModule.getProject().refresh();
 
 		// this.addComponent(ngModule, ngList);
-		addImport(ngModule, ngList.getName());
+		NgHelper.addImport(ngModule, ngList.getName());
 
 		return ngList;
 	}
@@ -285,7 +268,7 @@ public class AngularServiceImpl implements AngularService {
 		ngModule.getProject().refresh();
 
 		// this.addComponent(ngModule, ngDetail);
-		addImport(ngModule, ngDetail.getName());
+		NgHelper.addImport(ngModule, ngDetail.getName());
 
 		return ngDetail;
 
@@ -326,7 +309,7 @@ public class AngularServiceImpl implements AngularService {
 		ngModule.getProject().refresh();
 
 		// this.addComponent(ngModule, ngEdit);
-		addImport(ngModule, ngEdit.getName());
+		NgHelper.addImport(ngModule, ngEdit.getName());
 
 		return ngEdit;
 	}
@@ -359,7 +342,7 @@ public class AngularServiceImpl implements AngularService {
 			for (NgRoute r2 : r1.getChildren()) {
 				if (r2.getPath().equals(rootRoutePath)) {
 					r2.setChildren(ngCrud.genRoute());
-					updateRoute(ngModule, ngRoutes);
+					NgHelper.updateRoute(ngModule, ngRoutes);
 					return;
 				}
 			}
@@ -367,123 +350,123 @@ public class AngularServiceImpl implements AngularService {
 		NgRoute rootRoute = new NgRoute(rootRoutePath);
 		rootRoute.setChildren(ngCrud.genRoute());
 		ngRoutes.get(0).getChildren().add(rootRoute);
-		updateRoute(ngModule, ngRoutes);
+		NgHelper.updateRoute(ngModule, ngRoutes);
 	}
 
-	/*
-	 * Atualiza o array das rotas.
-	 */
-	private void updateRoute(NgModuleRepresentation ngModule, List<NgRoute> routes) {
-		String content = ngModule.getTsFileContent();
-		Pattern pattern = Pattern.compile(NgModuleRepresentation.ROUTE_PATTERN);
-		Pair<Integer, Integer> idxRoute = StringHelper.indexOfPattern(pattern, content);
-		String startRouteArray = content.substring(idxRoute.getRight());
-		Pair<Integer, Integer> idxOfFirstArray = StringHelper.indexOfFirstArray(startRouteArray);
+//	/*
+//	 * Atualiza o array das rotas.
+//	 */
+//	private void updateRoute(NgModuleRepresentation ngModule, List<NgRoute> routes) {
+//		String content = ngModule.getTsFileContent();
+//		Pattern pattern = Pattern.compile(NgModuleRepresentation.ROUTE_PATTERN);
+//		Pair<Integer, Integer> idxRoute = StringHelper.indexOfPattern(pattern, content);
+//		String startRouteArray = content.substring(idxRoute.getRight());
+//		Pair<Integer, Integer> idxOfFirstArray = StringHelper.indexOfFirstArray(startRouteArray);
+//
+//		String start = content.substring(0, idxRoute.getRight());
+//		String end = content.substring(idxRoute.getRight() + idxOfFirstArray.getRight(), content.length());
+//
+//		// @formatter:off
+//		PrettyPrinter pp = new DefaultPrettyPrinter()
+//				.withObjectIndenter(new DefaultIndenter("", " "))
+//				.withArrayIndenter(new DefaultIndenter())
+//				.withSpacesInObjectEntries();
+//		// @formatter:on
+//
+//		String newContent = start.concat(JsonHelper.serialize(routes, pp).replaceAll("\"", "'")).concat(end);
+//		this.save(ngModule, newContent);
+//	}
 
-		String start = content.substring(0, idxRoute.getRight());
-		String end = content.substring(idxRoute.getRight() + idxOfFirstArray.getRight(), content.length());
-
-		// @formatter:off
-		PrettyPrinter pp = new DefaultPrettyPrinter()
-				.withObjectIndenter(new DefaultIndenter("", " "))
-				.withArrayIndenter(new DefaultIndenter())
-				.withSpacesInObjectEntries();
-		// @formatter:on
-
-		String newContent = start.concat(JsonHelper.serialize(routes, pp).replaceAll("\"", "'")).concat(end);
-		this.save(ngModule, newContent);
-	}
-
-	private void addComponent(NgModuleRepresentation module, NgCrudRepresentation ngCrud) {
-		List<String> declarations = new ArrayList<>(module.getModuleDeclarations());
-		declarations.add(ngCrud.getList().getName());
-		declarations.add(ngCrud.getDetail().getName());
-		ngCrud.getEdit().ifPresent(ngEdit -> declarations.add(ngEdit.getName()));
-
-		String content = module.getTsFileContent();
-
-		Pattern pattern = Pattern.compile(NgModuleRepresentation.DECLARATION_PATTERN);
-		int idx = StringHelper.indexOfPattern(pattern, content).getRight();
-		String startDeclarationArray = content.substring(idx);
-		Pair<Integer, Integer> idxOfFirstArray = StringHelper.indexOfFirstArray(startDeclarationArray);
-		Pair<Integer, Integer> idxDeclarations = Pair.of(idx + 1, idx + idxOfFirstArray.getRight() - 1);
-
-		String start = content.substring(0, idxDeclarations.getLeft());
-		String end = content.substring(idxDeclarations.getRight(), content.length());
-		String newContent = start.concat("\n    ").concat(StringUtils.join(declarations, ",\n    ")).concat("\n  ").concat(end);
-		this.save(module, newContent);
-		return;
-	}
+//	private void addComponent(NgModuleRepresentation module, NgCrudRepresentation ngCrud) {
+//		List<String> declarations = new ArrayList<>(module.getModuleDeclarations());
+//		declarations.add(ngCrud.getList().getName());
+//		declarations.add(ngCrud.getDetail().getName());
+//		ngCrud.getEdit().ifPresent(ngEdit -> declarations.add(ngEdit.getName()));
+//
+//		String content = module.getTsFileContent();
+//
+//		Pattern pattern = Pattern.compile(NgModuleRepresentation.DECLARATION_PATTERN);
+//		int idx = StringHelper.indexOfPattern(pattern, content).getRight();
+//		String startDeclarationArray = content.substring(idx);
+//		Pair<Integer, Integer> idxOfFirstArray = StringHelper.indexOfFirstArray(startDeclarationArray);
+//		Pair<Integer, Integer> idxDeclarations = Pair.of(idx + 1, idx + idxOfFirstArray.getRight() - 1);
+//
+//		String start = content.substring(0, idxDeclarations.getLeft());
+//		String end = content.substring(idxDeclarations.getRight(), content.length());
+//		String newContent = start.concat("\n    ").concat(StringUtils.join(declarations, ",\n    ")).concat("\n  ").concat(end);
+//		this.save(module, newContent);
+//		return;
+//	}
 
 	/*
 	 *
 	 */
-	private void addNavigation(NgPageRepresentation page, NgCrudRepresentation ngCrud) {
-		List<NgPageNavigationRepresentation> navigations = new ArrayList<>(page.getNavigations());
+//	private void addNavigation(NgPageRepresentation page, NgCrudRepresentation ngCrud) {
+//		List<NgPageNavigationRepresentation> navigations = new ArrayList<>(page.getNavigations());
+//
+//		NgPageNavigationRepresentation ngPageNavigation = new NgPageNavigationRepresentation();
+//		ngPageNavigation.setText(ngCrud.getTargetEntity().getName());
+//		ngPageNavigation.setPath("/" + ngCrud.getTargetEntity().getApiPath());
+//		ngPageNavigation.setIcon("file-alt");
+//
+//		navigations.add(ngPageNavigation);
+//
+//		String content = page.getTsFileContent();
+//		Pattern pattern = Pattern.compile(NgPageRepresentation.NAVIGATION_PATTERN);
+//		int idx = StringHelper.indexOfPattern(pattern, content).getRight();
+//		String startNavigationArray = content.substring(idx);
+//		Pair<Integer, Integer> idxOfFirstArray = StringHelper.indexOfFirstArray(startNavigationArray);
+//		Pair<Integer, Integer> idxNavigations = Pair.of(idx, idx + idxOfFirstArray.getRight());
+//
+//		String start = content.substring(0, idxNavigations.getLeft());
+//		String end = content.substring(idxNavigations.getRight(), content.length());
+//		String newContent = start.concat(JsonHelper.serialize(navigations).replaceAll("\"", "'")).concat(end);
+//
+//		this.save(page, newContent);
+//		return;
+//
+//	}
+//
+//	@SneakyThrows
+//	private void addImport(NgModuleRepresentation module, String componentName) {
+//		// @formatter:off
+//		NgComponentRepresentation component = module.getProject().getNgComponents().stream()
+//				.filter(ngC -> ngC.getName().equals(componentName))
+//				.findAny()
+//				.orElseThrow(() -> new IllegalArgumentException(String.format("NgComponent %s não encontrado.", componentName)));
+//
+//		if (module.getImports()
+//				.stream()
+//				.flatMap(ngImport -> ngImport.getItems().stream())
+//				.noneMatch(ngImportName -> ngImportName.equals(component.getName()))) {
+//			// @formatter:on
+//			List<String> lines = Files.readAllLines(module.getPath(), StandardCharsets.UTF_8);
+//			Path importPath = module.getPath().getParent().relativize(component.getNgTsClass().getPath());
+//			// @formatter:off
+//			String importContent = TemplateBuilder.builder()
+//					.tpl("import { {{componentName}} } from './{{pathName}}';")
+//					.put("componentName", component.getName())
+//					.put("pathName", FilenameUtils.removeExtension(importPath.toString()))
+//					.build();
+//			// @formatter:on
+//			lines.add(module.getImports().size(), importContent.trim());
+//			this.save(module, StringUtils.join(lines, "\n"));
+//		}
+//	}
 
-		NgPageNavigationRepresentation ngPageNavigation = new NgPageNavigationRepresentation();
-		ngPageNavigation.setText(ngCrud.getTargetEntity().getName());
-		ngPageNavigation.setPath("/" + ngCrud.getTargetEntity().getApiPath());
-		ngPageNavigation.setIcon("file-alt");
-
-		navigations.add(ngPageNavigation);
-
-		String content = page.getTsFileContent();
-		Pattern pattern = Pattern.compile(NgPageRepresentation.NAVIGATION_PATTERN);
-		int idx = StringHelper.indexOfPattern(pattern, content).getRight();
-		String startNavigationArray = content.substring(idx);
-		Pair<Integer, Integer> idxOfFirstArray = StringHelper.indexOfFirstArray(startNavigationArray);
-		Pair<Integer, Integer> idxNavigations = Pair.of(idx, idx + idxOfFirstArray.getRight());
-
-		String start = content.substring(0, idxNavigations.getLeft());
-		String end = content.substring(idxNavigations.getRight(), content.length());
-		String newContent = start.concat(JsonHelper.serialize(navigations).replaceAll("\"", "'")).concat(end);
-
-		this.save(page, newContent);
-		return;
-
-	}
-
-	@SneakyThrows
-	private void addImport(NgModuleRepresentation module, String componentName) {
-		// @formatter:off
-		NgComponentRepresentation component = module.getProject().getNgComponents().stream()
-				.filter(ngC -> ngC.getName().equals(componentName))
-				.findAny()
-				.orElseThrow(() -> new IllegalArgumentException(String.format("NgComponent %s não encontrado.", componentName)));
-
-		if (module.getImports()
-				.stream()
-				.flatMap(ngImport -> ngImport.getItems().stream())
-				.noneMatch(ngImportName -> ngImportName.equals(component.getName()))) {
-			// @formatter:on
-			List<String> lines = Files.readAllLines(module.getPath(), StandardCharsets.UTF_8);
-			Path importPath = module.getPath().getParent().relativize(component.getNgTsClass().getPath());
-			// @formatter:off
-			String importContent = TemplateBuilder.builder()
-					.tpl("import { {{componentName}} } from './{{pathName}}';")
-					.put("componentName", component.getName())
-					.put("pathName", FilenameUtils.removeExtension(importPath.toString()))
-					.build();
-			// @formatter:on
-			lines.add(module.getImports().size(), importContent.trim());
-			this.save(module, StringUtils.join(lines, "\n"));
-		}
-	}
-
-	@SneakyThrows
-	private void save(NgModuleRepresentation module, String newContent) {
-		try (BufferedWriter writer = Files.newBufferedWriter(module.getPath())) {
-			writer.write(newContent);
-			writer.flush();
-		}
-	}
-
-	@SneakyThrows
-	private void save(NgPageRepresentation page, String newContent) {
-		try (BufferedWriter writer = Files.newBufferedWriter(page.getPath())) {
-			writer.write(newContent);
-			writer.flush();
-		}
-	}
+//	@SneakyThrows
+//	private void save(NgModuleRepresentation module, String newContent) {
+//		try (BufferedWriter writer = Files.newBufferedWriter(module.getPath())) {
+//			writer.write(newContent);
+//			writer.flush();
+//		}
+//	}
+//
+//	@SneakyThrows
+//	private void save(NgPageRepresentation page, String newContent) {
+//		try (BufferedWriter writer = Files.newBufferedWriter(page.getPath())) {
+//			writer.write(newContent);
+//			writer.flush();
+//		}
+//	}
 }
