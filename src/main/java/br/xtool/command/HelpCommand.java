c3317d793e7a0cf1786ec05cbf3d2354a8dfb035
jcruz <jribacruz@gmail.com>
2019-11-29 23:38:49 -0300

    Impl do comando Help

---
+++ src/main/java/br/xtool/command/HelpCommand.java
@@ -0,0 +1,28 @@
+package br.xtool.command;
+
+import br.xtool.annotation.CoreCommand;
+import br.xtool.command.core.AbstractCommand;
+import br.xtool.core.Console;
+import org.springframework.beans.factory.annotation.Autowired;
+import picocli.CommandLine.Command;
+
+/**
+ * Comando para limpar a tela do console.
+ */
+@CoreCommand
+@Command(name = "help", description = "Ajuda da aplicação")
+public class HelpCommand extends AbstractCommand {
+
+	@Autowired
+	private Console console;
+
+    @Override
+    public void run() {
+        getCommandList().entrySet().stream()
+                .filter(e -> e.getValue().getClass().isAnnotationPresent(CoreCommand.class))
+                .map(e -> e.getValue().getClass().getAnnotation(Command.class))
+                .filter(e -> !e.name().equals("help"))
+                .forEach(e -> console.println("@|blue %s|@ -- %s", e.name(), e.description()[0]));
+    }
+
+}
